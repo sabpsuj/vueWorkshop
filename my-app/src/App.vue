50e917aaf21bed093708191e09e05a18bdd093b8
Sabina Psuj <sabina.psuj@creativestyle.pl>
2018-09-08 11:42:14 +0200

    [#2.2]

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <ul>
         <li v-for="item in items" :key="item.id">{{ item.name }}
-            <button @click="removeItem(item.id)">{{removeButtonLabel}}</button>
+            <button @click="removeItem(item.id)">{{ removeButtonLabel }}</button>
         </li>
     </ul>
-    <button @click="addNew">{{buttonLabel}}</button>
+    
     <form @submit.prevent="onSubmit()">
-      <input id="fishInput" type="text"/>
+      <input 
+        name="item" 
+        id="fishInput" 
+        v-validate="'required:true'"
+        type="text"/>
+      <button>{{buttonLabel}}</button>
     </form>
     
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import uuid from 'uuid/v4';
 
 export default {
   name: 'app',
@@ -23,22 +28,18 @@ export default {
       items:[
             {
                 id: 0,
-                buttonId: 0,
                 name: 'Łosoś'
             },
             {
                 id: 1,
-                buttonId: 1,
                 name: 'Pstrąg'
             },
             {
                 id: 2,
-                buttonId: 2,
                 name: 'Flądra'
             },
             {
                 id: 3,
-                buttonId: 3,
                 name: 'Szprot'
             }
         ],
@@ -49,13 +50,24 @@ export default {
       methods: {
           addNew() {
               let newFish = document.getElementById('fishInput').value;
-              this.items.push({id: 4, name: newFish})
+              this.items.push({id: uuid(), name: newFish});
+              newFish.name = ''
           },
           removeItem(el) {
               this.items.splice(el, 1);
+          },
+          onSubmit() {
+            this.$validator.validateAll().then(result => {
+            if (!result) {
+              return;
+            }
+            this.addNew();
+            this.$validator.reset();
+            });
           }
-      }
-}
+        }
+  }
+
 </script>
 
 <style>
